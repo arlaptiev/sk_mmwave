b0ee642d03c10037b92ccdcdbefc4e9e7a757a67
fzaman500 <fzaman500@gmail.com>
2025-05-02 15:29:47 -0400

    template for one-snap

---
+++ one_snap_test.py
@@ -0,0 +1,24 @@
+# initialize phone socket
+
+
+# initialize radar connection
+
+
+# if phone presses snap, then run radar file 
+
+
+# collect data from radar
+
+
+# process data from radar with fft
+
+
+# find peaks with distances
+
+
+
+# check if there is a strong peak at an inside box distance
+
+
+# send boolean back to phone if box empty or not
+
