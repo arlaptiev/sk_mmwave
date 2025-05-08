bd450a7be7597a8916ded3659a797c86a0e7138e
Artem <arlaptiev@gmail.com>
2025-05-07 21:04:08 -0400

    fix-live-fft

--- live_fft.py
+++ live_fft.py
@@ -17,28 +17,12 @@ from nodes.radar import Radar
 
 
 
-plt.ion()   # interactive plotting
+# Global variables for plot handles (to update instead of redrawing)
 fig, (ax_time, ax_freq) = plt.subplots(2, 1, figsize=(12, 9))
+line_time, = ax_time.plot([], [])
+line_freq, = ax_freq.plot([], [])
 
-# colors and labels
-rx_colors = ['blue', 'orange', 'green', 'red']
-rx_labels = ['RX1', 'RX2', 'RX3', 'RX4']
-
-# create line objects
-line_time = [ax_time.plot([], [], color=rx_colors[i], label=rx_labels[i], alpha=0.7)[0] for i in range(4)]
-line_freq = [ax_freq.plot([], [], color=rx_colors[i], label=rx_labels[i], alpha=0.7)[0] for i in range(4)]
-
-# set titles and labels
-ax_time.set_title('Time Domain Signals')
-ax_time.set_xlabel('Sample Index')
-ax_time.set_ylabel('Amplitude')
-ax_time.legend()
-
-ax_freq.set_title('Frequency Domain Signals')
-ax_freq.set_xlabel('Distance (m)')
-ax_freq.set_ylabel('Magnitude')
-ax_freq.legend()
-
+plt.ion()  # Enable interactive mode
 plt.show()
 
 
@@ -47,38 +31,51 @@ def display_frame(message):
     frame = message.get("data", None)
     params = message.get("params", None)
 
-    # radar params
-    c = 3e8                                         # speed of light - m/s
-    SAMPLES_PER_CHIRP = params['n_samples']         # adc number of samples per chirp
-    SAMPLE_RATE = params['sample_rate']             # digout sample rate in Hz
-    FREQ_SLOPE = params['chirp_slope']              # frequency slope in Hz (/s)
+    # Radar parameters
+    c = 3e8
+    SAMPLES_PER_CHIRP = params['n_samples']
+    SAMPLE_RATE = params['sample_rate']
+    FREQ_SLOPE = params['chirp_slope']
 
+    # Processing
+    avg_chirps = np.mean(frame, axis=0)  # shape: (n_samples, n_rx)
+    signal = avg_chirps
 
-    # -- Preprocess the frame --
-    # average over chirps
-    signal = np.mean(frame, axis=0)                 # shape (n_samples, n_rx)
-
-
-    # -- Compute Range FFT --
-    fft_result = fft(signal, axis=0)                # shape (n_samples, n_rx)
-    fft_freqs = fftfreq(SAMPLES_PER_CHIRP, 1 / SAMPLE_RATE)
+    fft_result = fft(signal, axis=0)
+    fft_freqs = fftfreq(SAMPLES_PER_CHIRP, 1/SAMPLE_RATE)
     fft_meters = fft_freqs * c / (2 * FREQ_SLOPE)
     half_range = SAMPLES_PER_CHIRP // 2
 
 
-    # -- Plotting --
-    for i in range(min(4, signal.shape[1])):
-        line_time[i].set_data(np.arange(SAMPLES_PER_CHIRP), signal[:, i])
-        line_freq[i].set_data(fft_meters[:half_range], np.abs(fft_result[:half_range, i]))
+    # Clear and replot
+    ax_time.cla()
+    ax_freq.cla()
 
-    # adjust axes limits
+    colors = ['blue', 'orange', 'green', 'red']
+    labels = ['RX1', 'RX2', 'RX3', 'RX4']
+
+    # Time Domain Plot
+    for rx in range(signal.shape[1]):
+        ax_time.plot(signal[:, rx], color=colors[rx], label=labels[rx], alpha=0.7)
+    ax_time.set_title('Time Domain Signals')
+    ax_time.set_xlabel('Sample Index')
+    ax_time.set_ylabel('Amplitude')
+    ax_time.legend()
     ax_time.set_xlim(0, SAMPLES_PER_CHIRP)
-    # ax_time.set_ylim(np.min(signal), np.max(signal))
 
+    # Frequency Domain Plot
+    for rx in range(signal.shape[1]):
+        ax_freq.plot(fft_meters[:half_range], np.abs(fft_result[:half_range, rx]), color=colors[rx], label=labels[rx], alpha=0.7)
+    ax_freq.set_title('Frequency Domain Signals')
+    ax_freq.set_xlabel('Distance (m)')
+    ax_freq.set_ylabel('Magnitude')
+    ax_freq.legend()
     ax_freq.set_xlim(0, 1.5)
-    # ax_freq.set_ylim(0, np.max(np.abs(fft_result[:half_range, :])) * 1.1)
+    # ax_freq.set_ylim(0, np.max(np.abs(fft_result[:half_range])) * 1.1)
 
+    plt.tight_layout()
     plt.pause(0.001)
+
 
 
 
