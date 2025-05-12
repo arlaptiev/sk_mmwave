5187d64b657c00e5a6c4d619c95a3782a687047d
maisy <mllam@mit.edu>
2025-05-11 23:26:15 -0400

    added send to phone functionality

--- demo.py
+++ demo.py
@@ -13,10 +13,6 @@ from nodes.phone_node import Lidar
 from src.lidar_dsp import detect_planes_and_box
 
 
-# TODO add the send to phone functionality
-
-
-
 # ==== initialize radar and set params
 args = SimpleNamespace(**{
   'cfg': 'configs/1443_mmwavestudio_config_continuous.lua',
@@ -39,72 +35,68 @@ lidar = Lidar()
 img, depth_map, meta = lidar.read()
 floor_dist, box_dist, table_dist = detect_planes_and_box(img, depth_map, meta)
 print('Table distance', table_dist)
-# UDP_IP = "0.0.0.0"  # Listen on all interfaces
-# UDP_PORT = 5005
-
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# sock.bind((UDP_IP, UDP_PORT))
-
-# print(f"Listening on UDP {UDP_PORT}...")
-
-# addr = ""
-
-# # if phone presses snap, then run radar file 
-# while True:
-#     data, addr = sock.recvfrom(1024)
-#     message = data.decode().strip()
-#     print(f"Received message from {addr}: {message}")
-#     if message == "RADAR STARTING DATA CAPTURE NOW":
-#         break
-    
-
-def send_to_phone(message, addr, sock):
-    sock.sendto(message.encode(), addr)
-    print(f"Sent message")
-
-
 
 
 # ==== collect data from radar
 frame = radar.read()
+radar.close()
 # avg over chirps
 avg_chirps = np.mean(frame, axis=0)     # shape: (num_samples, num_rx)
 # choose rx0
-signal = avg_chirps[:, 0]               # shape: (num_samples,)
+signal = avg_chirps                     # shape: (num_samples, num_rx)
 
 
 # ==== process data from radar with fft
-fft_result = fft(signal)
-fft_magnitude = np.abs(fft_result[:SAMPLES_PER_CHIRP // 2])
+fft_result = fft(signal, axis=0)
+rx = 0
+fft_magnitude = np.abs(fft_result[:SAMPLES_PER_CHIRP // 2, rx])
 fft_freqs = fftfreq(SAMPLES_PER_CHIRP, 1/SAMPLE_RATE)
 fft_meters = fft_freqs[:SAMPLES_PER_CHIRP // 2] * c / (2 * FREQ_SLOPE)
 
 # ==== find low and high bounds from lidar data
-low_bound = floor_dist
-high_bound = table_dist
+low_bound = table_dist
+high_bound = table_dist + .35
+
 
 # ==== find peaks with distances
 range_mask = (fft_meters >= low_bound) & (fft_meters <= high_bound)
-fft_magnitude = fft_magnitude[range_mask]
-fft_meters = fft_meters[range_mask]
+fft_magnitude_masked = fft_magnitude[range_mask]
+fft_meters_masked = fft_meters[range_mask]
 
 
 # ==== check if there is a strong peak at an inside box distance
 THRESHOLD = 450000
-detection = any(fft_magnitude > THRESHOLD)
+detection = (fft_magnitude_masked > THRESHOLD).any()
+masked_max = np.max(fft_magnitude_masked)
+print('Max within range:', masked_max)
 
 
 # ==== send boolean back to phone if box empty or not
+message = "TRUE" if detection else "FALSE"
 print("Detection", detection)
-phone_ip = "192.168.41.178"
-phone_port = 6000
-UDP_IP = "0.0.0.0"  # Listen on all interfaces
-UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
-#data, addr = sock.recvfrom(1024)
-if detection:
-  send_to_phone("TRUE", lidar.prev_addr, sock)
-else:
-  send_to_phone("FALSE", lidar.prev_addr, sock)
+def send_to_phone(message, ip, port):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.sendto(message.encode(), (ip, port))
+  sock.close()
+
+ip = "192.168.41.62"
+port = 9999
+send_to_phone(message, ip, port)
+
+plt.figure(figsize=(12, 9))
+plt.plot(fft_meters,  np.abs(fft_result[:SAMPLES_PER_CHIRP // 2, 0]), color='blue', label='RX1', alpha=0.7)
+plt.plot(fft_meters,  np.abs(fft_result[:SAMPLES_PER_CHIRP // 2, 1]), color='orange', label='RX2', alpha=0.7)
+plt.plot(fft_meters,  np.abs(fft_result[:SAMPLES_PER_CHIRP // 2, 2]), color='green', label='RX3', alpha=0.7)
+plt.plot(fft_meters,  np.abs(fft_result[:SAMPLES_PER_CHIRP // 2, 3]), color='red', label='RX4', alpha=0.7)
+plt.vlines([low_bound, high_bound], [0, 0], [masked_max + 1000, masked_max + 1000])
+plt.title('Frequency Domain Signals')
+plt.xlabel('Distance (m)')
+plt.ylabel('Magnitude')
+plt.legend()
+plt.xlim(0,1.5)
+
+# plt.show()
+
+
+
